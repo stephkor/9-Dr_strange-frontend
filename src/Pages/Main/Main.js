6dd7a251006950e663cb0ec48ae42181a98e3f53
solmii <rkdthfal4759@naver.com>
2020-06-22 17:46:11 +0900

    [Add] 초기세팅 - CRA 초기세팅

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <section className="main">
+        <p>Dr_strange 화이팅!!</p>
+      </section>
+    );
+  }
+}
+
+export default Main;
