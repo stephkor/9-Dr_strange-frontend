a6187ec0989a41976b9415fc596a9bae19f47b32
solmii <rkdthfal4759@naver.com>
2020-06-23 15:32:41 +0900

    [Refactor] js, scss리팩토링 - common.scss 수정 및 리팩토링 - Nav.js 리팩토링

--- src/Components/Nav/CustomerMenu.js
+++ src/Components/Nav/CustomerMenu.js
@@ -3,7 +3,6 @@ import "./Nav.scss";
 
 class CustomerMenu extends React.Component {
   render() {
-<<<<<<< HEAD
     // 비구조화 할당. ES6. Destructuring.
     const { width, height, view, path } = this.props;
     return (
@@ -11,17 +10,6 @@ class CustomerMenu extends React.Component {
         <button>
           <svg style={{ width: width, height: height }} viewBox={view}>
             <path d={path} />
-=======
-    return (
-      <li className="CustomerMenu">
-        <button>
-          <svg
-            width={this.props.width}
-            height={this.props.height}
-            viewBox={this.props.view}
-          >
-            <path d={this.props.path} />
->>>>>>> 292ac9b6a4688b6d7ecbeb7f08fa3a3691501fb1
           </svg>
         </button>
       </li>
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "Components/Nav/Nav";
 import Footer from "Components/Footer/Footer";
 import "styles/reset.scss";
+import "styles/common.scss";
 
 class Main extends React.Component {
   render() {
