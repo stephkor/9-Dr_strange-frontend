f3fabf7b799abfc334a29617d8a3295e994ab299
solmii <rkdthfal4759@naver.com>
2020-06-23 14:58:46 +0900

    [Add] common 스타일 추가 - common.scss에 기본 스타일지정 -Nav.js Refactor

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.scss";
+import "styles/reset.scss";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="Footer">
+        <div className="footer_top">
+          <div></div>
+        </div>
+        <div className="footer_bottom"></div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
