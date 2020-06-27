94ac9f395e453dd230ad359cef13e1352fbf93c8
solmii <rkdthfal4759@naver.com>
2020-06-27 23:00:27 +0900

    [Add] 후기, 문의, subtitle 등 component 구현 - 상품 후기 : 접었을때와 폈을때 다른 데이터 및 레이아웃 구현 - 환불 안내 : 버튼으로 접었다 펼 수 있는 기능 구현 - 클릭시 페이지 최상단으로 이동하는 버튼 구현

---
+++ src/Pages/ProductDetail/ProductForm.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { MINUS, PLUS } from "config";
+
+class ProductForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      origin: this.props.originPrice,
+      sale: this.props.salePrice,
+      currentQuantity: 1,
+    };
+  }
+
+  // 수량 input에 직접 갯수 수정
+  setInputHandler = (e) => {
+    this.setState({
+      currentQuantity: e.target.value,
+    });
+  };
+
+  // minus, plus 버튼 클릭시 수량 및 가격 변동
+  itemCntrolHandler = () => {
+    console.log("ok!");
+  };
+
+  render() {
+    const { currentQuantity, origin, sale } = this.state;
+
+    return (
+      <div className="product_price_form">
+        <div className="product_quantity">
+          <p>수량</p>
+          <input
+            className="main-font"
+            type="text"
+            value={currentQuantity}
+            onChange={this.setInputHandler.bind(this)}
+          />
+          <button>
+            <img alt="-" className="btn_minus" src={MINUS} />
+          </button>
+          <button>
+            <img alt="+" className="btn_plus" src={PLUS} />
+          </button>
+        </div>
+        <div className="product_item_price num-font">
+          <span className="origin_price">{origin.toLocaleString()}</span>
+          <span className="sale_price">{sale.toLocaleString()}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductForm;
---
+++ src/Pages/ProductDetail/ReviewBoard.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Path from "Components/Path";
+import { GUEST_ICON, REVIEW_IMG } from "config";
+
+class ReviewBoard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isActive: false,
+    };
+  }
+  openReviewHandler = () => {
+    this.setState({
+      isActive: !this.state.isActive,
+    });
+  };
+  render() {
+    const { isActive } = this.state;
+    const { name, title, img, size, rating, content } = this.props;
+    return (
+      <div className="ReviewBoard">
+        <div className="review_board_top">
+          <img alt="guest_icon" src={GUEST_ICON}></img>
+          <p className="font-b">{name}</p>
+        </div>
+        <div className="review_board_title">
+          <h3 className="font-b">{title}</h3>
+          {img ? (
+            <Path width="24" height="21" view="0 0 56 48" path={REVIEW_IMG} />
+          ) : (
+            ""
+          )}
+        </div>
+
+        {isActive && img ? (
+          <div className="review_img">
+            <img alt={title} src={img} />
+          </div>
+        ) : (
+          ""
+        )}
+
+        <div className="review_board_bottom">
+          <span>{"★".repeat(rating)}</span>
+          {isActive ? (
+            <div className="review_content">
+              <p>
+                구매 사이즈<span>|</span>
+                {size}
+              </p>
+              <p>{content}</p>
+            </div>
+          ) : (
+            ""
+          )}
+          <button className="underline font-b" onClick={this.openReviewHandler}>
+            {isActive ? "접기" : "펼쳐보기"}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ReviewBoard;
