1a6b1f57861de7e0972d72dc45414d7bf6c7901a
solmii <rkdthfal4759@naver.com>
2020-06-28 17:30:00 +0900

    [Add] product list 페이지 구현 및 세부 product component 구현 - index 값에 따라 다른 사이즈로 출력되는 component 제작 - mock data 만든 후 map으로 component 출력

--- src/Pages/ProductList/ProductList.js
+++ src/Pages/ProductList/ProductList.js
@@ -1,21 +1,50 @@
 import React from "react";
 import "./ProductList.scss";
+import Nav from "Components/Nav/Nav";
+import ProductListMap from "./ProductListMap";
+import Footer from "Components/Footer/Footer";
 
 class ProductList extends React.Component {
   constructor() {
     super();
+    this.state = {
+      productListData: [],
+    };
   }
+  // product list data 받아오기
   componentDidMount() {
-    fetch("http://localhost:3000/data/productDetailInfo.json")
+    fetch("http://localhost:3000/data/productListInfo_img1.json")
       .then((res) => res.json())
       .then((res) =>
         this.setState({
-          productData: res.productDetailInfo,
+          productListData: res.productListInfo_img1,
         })
       );
   }
   render() {
-    return <div></div>;
+    return (
+      <>
+        <Nav />
+        <div className="ProductList m-w-1140 m-auto">
+          {this.state.productListData[0] &&
+            this.state.productListData.map((product, idx) => (
+              <>
+                <ProductListMap
+                  color={product.color}
+                  name={product.productName}
+                  like={product.like}
+                  originPrice={product.originPrice}
+                  salePrice={product.salePrice}
+                  img={product.productImg}
+                  index={idx + 1}
+                  key={idx}
+                />
+              </>
+            ))}
+        </div>
+        <Footer />
+      </>
+    );
   }
 }
 
---
+++ src/Pages/ProductList/ProductListMap.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./ProductList.scss";
+import ListWishButton from "Pages/ProductList/ListWishButton";
+
+class ProductListMap extends React.Component {
+  render() {
+    const {
+      color,
+      name,
+      like,
+      originPrice,
+      salePrice,
+      img,
+      index,
+    } = this.props;
+    return (
+      <div className="ProductListMap">
+        {img[2] ? (
+          <div className="product_small">
+            <img alt={name} src={img} />
+          </div>
+        ) : index % 9 === 0 ? (
+          <div className="product_large">
+            <img alt={name} src={img[0]} />
+            <img alt={name} src={img[1]} />
+          </div>
+        ) : (
+          <div className="product_medium">
+            <img alt={name} src={img[0]} />
+            <img alt={name} src={img[1]} />
+          </div>
+        )}
+        <div className="product_list_info">
+          <div className="list_info_content">
+            <h2>{name}</h2>
+            <p className="product_color">{color}</p>
+            <ListWishButton like={like} />
+          </div>
+          <div className="list_info_option">
+            {this.props.salePrice ? (
+              <>
+                <p className="sale_price num-font">{salePrice}</p>
+                <p className="origin_price_ws num-font">{originPrice}</p>
+              </>
+            ) : (
+              <p className="origin_price num-font">
+                {originPrice.toLocaleString()}
+              </p>
+            )}
+            <button>장바구니 담기</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default ProductListMap;
