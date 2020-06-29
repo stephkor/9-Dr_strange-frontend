0f1ffbc5c92f6ed281adc8a587d72930a6e674a8
한 <han@Han-ui-Mac-Book-Air.local>
2020-06-29 15:12:22 +0900

    pull 받기 전 임시저장

--- src/Pages/Main/Main_sumin/ProductFilter.js
+++ src/Pages/Main/Main_sumin/ProductFilter.js
@@ -3,11 +3,24 @@ import ProductItemSlider from "./ProductItemSlider.js";
 import "./ProductFilter.scss";
 
 class ProductFilter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      product: "",
+    };
+  }
+
+  //   componentDidMount() {
+  //     fetch("localhost:3000/data/Category.json")
+  //       .then((res) => res.json())
+  //       .then((res) => this.setState({ product: res.Category }));
+  //   }
+
   render() {
     return (
       <section className="filter_wrapper">
         <div className="ProductFilter">
-          <h2 className="filter_title">베스트셀러</h2>
+          <h2 className="filter_title">여성</h2>
           <nav className="filter_category">
             <ul>
               <li className="category">
