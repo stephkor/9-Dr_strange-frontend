51c39d531fa3cb62612b5de028bd140a199683b1
한 <han@Han-ui-Mac-Book-Air.local>
2020-07-02 21:28:59 +0900

    브랜치 이동 전 임시저장

--- src/Pages/Cart/EmptyCart.js
+++ src/Pages/Cart/EmptyCart.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Path from "Components/Path";
+import { PATH_BACK } from "config";
 
 class EmptyCart extends React.Component {
   render() {
     return (
       <section className="EmptyCart">
+        <div className="cart_header">
+          <button>
+            <Path width="34" height="22" view="0 0 34 22" path={PATH_BACK} />
+          </button>
+          <h1 className="cart_header_title">장바구니</h1>
+        </div>
         <div className="empty_icon">
           <svg
             viewBox="0 0 165 229"
